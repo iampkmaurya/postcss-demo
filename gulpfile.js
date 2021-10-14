0b06cb15736d6f80093268ce465ec4c2f796a44d
Prashant Maurya <prashantmaurya18@yahoo.in>
2021-10-14 10:43:08 +0530

    initial push

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+gulp.task('css', () => {
+    const postcss = require('gulp-postcss')
+    const sourcemaps = require('gulp-sourcemaps')
+
+    return gulp.src('css/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([require('autoprefixer')]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('build/'))
+})
+gulp.task('watch', function () {
+    gulp.watch('css/*.css', ['css']);
+});
